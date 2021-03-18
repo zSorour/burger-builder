e78119530d8859dbb2e1b458b01bf16d3af3fe74
zSorour <47137234+zSorour@users.noreply.github.com>
2021-03-18 16:18:33 +0200

    Updated Orders UI

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -16,14 +16,8 @@ const order = ( props ) => {
 
     const ingredientOutput = ingredients.map(ig => {
         return <span 
-            style={{
-                textTransform: 'capitalize',
-                display: 'inline-block',
-                margin: '0 8px',
-                border: '1px solid #ccc',
-                padding: '5px'
-                }}
-            key={ig.name}>{ig.name} ({ig.amount})</span>;
+           
+            key={ig.name}>{ig.name} ({ig.amount}) </span>;
     });
 
     return (
